bcdd613c1069499b027b5ea1d9da711bf132a4b7
AKMB-dev <assimakmb@gmail.com>
2021-04-30 09:31:15 +0000

    restored the light theme as default theme

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-console.log('Its working')
-
 let theme = localStorage.getItem('theme')
 
 if(theme == null){
-	setTheme('blue')
+	setTheme('light')
 }else{
 	setTheme(theme)
 }
@@ -14,18 +12,17 @@ let themeDots = document.getElementsByClassName('theme-dot')
 for (var i=0; themeDots.length > i; i++){
 	themeDots[i].addEventListener('click', function(){
 		let mode = this.dataset.mode
-		console.log('Option clicked:', mode)
 		setTheme(mode)
 	})
 }
 
 function setTheme(mode){
 	if(mode == 'light'){
-		document.getElementById('theme-style').href = 'light.css'
+		document.getElementById('theme-style').href = 'default.css'
 	}
 
 	if(mode == 'blue'){
-		document.getElementById('theme-style').href = 'default.css'
+		document.getElementById('theme-style').href = 'blue.css'
 	}
 
 	if(mode == 'green'){
